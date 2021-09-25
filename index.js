bb81dd3ae0174041226ecdafb336e122869abfa9
Al <alexseier12345@gmail..com>
2021-09-25 16:38:31 +0300

    done hm1

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const colors = require("colors");
+
 const isSimpleNumber = (num) => {
   for (let i = 2; i < num; i++) {
     if (num % i === 0) return false;
@@ -6,13 +8,43 @@ const isSimpleNumber = (num) => {
   return true;
 };
 
-const simpleNumbers = (downLimit, upLimit) => {
+const paint = (() => {
+  let colorNumber = 1;
+  return (num) => {
+    if (colorNumber > 3) colorNumber = 1;
+    switch (colorNumber) {
+      case 1:
+        console.log(colors.green(num));
+        break;
+      case 2:
+        console.log(colors.yellow(num));
+        break;
+      case 3:
+        console.log(colors.red(num));
+        break;
+    }
+    colorNumber++;
+  };
+})();
 
-    if(downLimit < 2) downLimit = 2
+const simpleNumbers = (downLimit, upLimit) => {
+  if (!Number.isInteger(downLimit) || !Number.isInteger(upLimit)) {
+    return console.log(colors.red("limits are not numbers"));
+  }
+  if (downLimit < 2) downLimit = 2;
+
+  let numbersAmount = 0;
 
   for (let i = downLimit; i <= upLimit; i++) {
-     if (isSimpleNumber(i)) console.log(i);
+    if (isSimpleNumber(i)) {
+      paint(i);
+      numbersAmount++;
+    }
   }
+
+  if (numbersAmount) {
+    console.log(colors.blue(`Amount of simple numbers: ${numbersAmount}`));
+  } else console.log(colors.red(`limit don't have simple numbers`));
 };
 
 simpleNumbers(+process.argv[2], +process.argv[3]);
