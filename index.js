efcc0909fc2aba1cb6129e5e4b6e8b13a91931b7
Al <alexseier12345@gmail..com>
2021-10-01 19:38:04 +0300

    done hm2

--- index.js
+++ index.js
@@ -1,50 +1,38 @@
-const colors = require("colors")
+const {formatDistanceToNowStrict, compareAsc} = require('date-fns')
+const EventEmitter = require('events');
 
-const isSimpleNumber = (num) => {
-  for (let i = 2; i < num; i++) {
-    if (num % i === 0) return false
-  }
+const timer = (date) => {
 
-  return true
+    const dateArray = date.split('-')
+    const finishedDate = new Date(...dateArray.reverse())
+
+    const isValidDate = compareAsc(finishedDate, new Date())
+    if (isValidDate === -1) return console.log('Please input correct date')
+
+    const emitter = new EventEmitter()
+
+    class Handler {
+        static next(payload) {
+            console.log(`${payload} left`)
+        }
+        static finish(payload) {
+            console.log('finish')
+            clearInterval(payload)
+        }
+    }
+
+    const intervalId = setInterval(() => {
+        console.clear()
+        const result = formatDistanceToNowStrict(finishedDate)
+        if (+result[0] === 0) {
+            emitter.emit('finish', intervalId)
+        } else emitter.emit('next', result)
+
+    }, 1000)
+
+    emitter.on('next', Handler.next)
+    emitter.on('finish', Handler.finish)
+
 }
 
-const paint = (() => {
-  let colorNumber = 1
-  return (num) => {
-    if (colorNumber > 3) colorNumber = 1
-    switch (colorNumber) {
-      case 1:
-        console.log(colors.green(num))
-        break
-      case 2:
-        console.log(colors.yellow(num))
-        break
-      case 3:
-        console.log(colors.red(num))
-        break
-    }
-    colorNumber++
-  }
-})()
-
-const simpleNumbers = (downLimit, upLimit) => {
-  if (!Number.isInteger(downLimit) || !Number.isInteger(upLimit)) {
-    return console.log(colors.red("limits are not numbers"))
-  }
-  if (downLimit < 2) downLimit = 2
-
-  let numbersAmount = 0
-
-  for (let i = downLimit; i <= upLimit; i++) {
-    if (isSimpleNumber(i)) {
-      paint(i)
-      numbersAmount++
-    }
-  }
-
-  if (numbersAmount) {
-    console.log(colors.blue(`Amount of simple numbers: ${numbersAmount}`))
-  } else console.log(colors.red(`limit don't have simple numbers`))
-}
-
-simpleNumbers(+process.argv[2], +process.argv[3])
+timer(process.argv[2])
