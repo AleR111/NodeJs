3dd2a6786d96701d0df465d94b7fb13b7ac1dbe8
Al <alexseier12345@gmail..com>
2021-10-21 23:31:13 +0300

    done hm5

--- hm5/index.js
+++ hm5/index.js
@@ -1,16 +1,18 @@
 #!/usr/bin/env node
 
 const fs = require("fs")
-const inquirer = require("inquirer")
 const http = require("http")
 const url = require("url")
-const yargs = require("yargs")
 const path = require("path")
 
-const executorDir = path.join(process.cwd())
+let fullPath = process.cwd()
 
 const dirFiles = (dir) => {
-  const list = fs.readdirSync(dir)
+  if (dir) fullPath = path.join(fullPath, dir)
+  if (fs?.lstatSync(fullPath)?.isFile()) {
+    return fs.readFileSync(fullPath, "utf-8")
+  }
+  const list = fs.readdirSync(fullPath)
 
   if (!list.length) return "directory is empty"
 
@@ -19,31 +21,32 @@ const dirFiles = (dir) => {
 
 const htmlPath = path.join(__dirname, "./public/index.html")
 
+const reqUrl = (url) => {
+  const cutIdx = url.indexOf("/?")
+  if (cutIdx === -1) return url
+  return url.slice(0, cutIdx)
+}
+
 const server = http.createServer((req, res) => {
   const { query } = url.parse(req.url, true)
-  console.log(req.url)
 
-  if (req.method === "GET") {
-    if (query?.we === "12") res.end("kek")
-    switch (req.url) {
-      case "/load":
-        res.setHeader("Content-Type", "application/json")
-        res.end(JSON.stringify(dirFiles(executorDir)))
-        break
-      case "/getData":
-        res.setHeader("Content-Type", "application/json")
-        res.end(JSON.stringify([1, 2, 9]))
-        break
-      default:
-        if (Object.keys(query).length === 0) {
-          console.log("dfgfdg")
-          const readHtml = fs.createReadStream(htmlPath)
-          res.writeHead(200, {
-            "Content-type": "text/html",
-          })
-          readHtml.pipe(res)
-        }
-    }
+  switch (reqUrl(req.url)) {
+    case "/load":
+      res.setHeader("Content-Type", "application/json")
+      res.end(JSON.stringify(dirFiles()))
+      break
+    case "/getData":
+      res.setHeader("Content-Type", "application/json")
+      res.end(JSON.stringify(dirFiles(query.path)))
+      break
+    default:
+      if (Object.keys(query).length === 0) {
+        const readHtml = fs.createReadStream(htmlPath)
+        res.writeHead(200, {
+          "Content-type": "text/html",
+        })
+        readHtml.pipe(res)
+      }
   }
 })
 
--- hm5/public/index.js
+++
@@ -1,7 +0,0 @@
-const url = "/getData"
-
-const data = fetch(url)
-  .then((result) => result.json)
-  .catch(console.log)
-
-console.log(222)
