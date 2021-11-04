8aebb81af8eb35fc10e645b03728de7c02e43c00
Al <alexseier12345@gmail..com>
2021-11-04 15:08:05 +0300

    done disconnect

--- hm6/index.js
+++ hm6/index.js
@@ -11,12 +11,34 @@ const server = http.createServer((req, res) => {
     });
     readStream.pipe(res);
 });
+
+const randomUser = () => Math.floor(Math.random() * 100)
+
+class User {
+    constructor() {
+        this.name = `user${randomUser()}`
+    }
+}
+
 const io = socket(server);
+let count = 0
 
 io.on('connection', (client) => {
     console.log('connection')
+    count++
+    const user = new User()
+
+    client.broadcast.emit('new-user', user, count)
+    client.emit('new-user', user, count)
+
+    client.on('disconnect', () => {
+        count--
+        client.broadcast.emit('disconnect-user', user, count)
+        client.emit('disconnect-user', user, count)
+    });
 
     client.on('client-msg', (data) => {
+        data.userName = user.name
         client.broadcast.emit('server-msg', data)
         client.emit('server-msg', data);
     })
