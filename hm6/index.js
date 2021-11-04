ae0ef204c55bfa0bf8a0ba0de747ca5c6e64bd8d
Al <alexseier12345@gmail..com>
2021-11-04 12:09:12 +0300

    create server

---
+++ hm6/index.js
@@ -0,0 +1,20 @@
+const socket = require('socket.io')
+const http = require('http')
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    const indexPath = path.join(__dirname, 'index.html');
+    const readStream = fs.createReadStream(indexPath);
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    readStream.pipe(res);
+});
+const io = socket(server);
+
+io.on('connection', (client) => {
+    console.log('connection')
+})
+
+server.listen(8080);
